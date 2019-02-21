e9723173df2eac4b988954f5a29fc99690eaea9a
david7-98 <44866373+david7-98@users.noreply.github.com>
2019-02-21 14:06:57 +0100

    Revert "Final"

--- pogama.cpp
+++ pogama.cpp
@@ -1,37 +1,5 @@
 //Holi
 
-
-/**
-
-* @brief Mateo eres malisimo jugando al Apex
-* @param ya ves loco
-
-
-*/
-
-
-void algoritmoDeOrdenacion (Alumno &array[], int util_array){
-
-	int aux = 0;
-
-	for(int i = 0; i < util_array; i++){
-
-		for(int j = 0; j < util_array; j++){
-
-			if(array.edad[i] < array.edad[j]){
-
-				aux = array.edad[i];
-				array.edad[i] = array.edad[j];
-				array.edad[j] = aux;
-
-			}
-
-		}
-	}
-
-}
-
 void imprimeAlumno(const Alumno &alu){
 	cout << alu.nombre << " " << alu.edad << endl;
 }
-
