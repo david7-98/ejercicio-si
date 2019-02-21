f3600966a5974a24115fe9e4e5fb9d0e85812cfb
Patricia Burgos <patriciaburgos.1999@gmail.com>
2019-02-21 14:06:43 +0100

    patri LeerStruct

--- pogama.cpp
+++ pogama.cpp
@@ -1,4 +1,20 @@
-//Holi
+void LeerStruct (Alumno &a) {
+
+	int num;
+	
+	do {
+		cout << ¿Cuántos alumnos desea? 
+		cin >> num;
+	} while (num < 0);
+
+	for (int i = 1; i <= num; i++) {
+		cout << "Alumno: " << i << endl;
+		cout << "Nombre: ";
+		cin >> a.nombre;
+		cout << "Edad: ";
+		cin >> a.edad;
+	}
+}
 
 void imprimeAlumno(const Alumno &alu){
 	cout << alu.nombre << " " << alu.edad << endl;
